a2d471d451121776ba4721f94a783112b3adc8af
ccm-chucky <charlescmatovu@gmail.com>
2021-08-30 11:25:01 -0400

    first part

--- dojo_blog-react/src/Home.js
+++ dojo_blog-react/src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Home = ()=>{
+    let [name,setName] = useState('charles');
+    const handleClick = (e,name) =>{
+        console.log('im clicked by '+name);
+        setName('matox');
+       
+    }
     return (
         <div className='home'>
             <h2>Homepage</h2>
+            <p>{name}</p>
+            <button onClick={(e)=>{handleClick(e,'matovu')}}>Click me</button>
         </div>
     );
 }
