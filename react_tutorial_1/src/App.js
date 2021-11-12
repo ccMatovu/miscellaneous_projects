a4edaf2071c0bcd2c10714d1402818429e8ca5e6
ccm-chucky <charlescmatovu@gmail.com>
2021-11-12 14:42:46 -0500

    useEffect

--- react_tutorial_1/src/App.js
+++ react_tutorial_1/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/4-useState-object'
-import Final from './tutorial/1-useState/final/2-useState-basics'
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+//import Final from './tutorial/1-useState/final/2-useState-basics'
 
 function App() {
   return (
@@ -8,7 +8,6 @@ function App() {
       
       <div>
         <Setup/>
-        <Final></Final>
       </div>
       
     </div>
