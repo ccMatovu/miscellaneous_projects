de2614eb4eb79e8e96e2fbd5b63331fe5d815686
ccm-chucky <charlescmatovu@gmail.com>
2021-11-17 20:52:54 -0500

    react projects

---
+++ react-projects/01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>
+          {people.length} Birthdays on List
+          <List people={people} />
+          <button onClick={() => setPeople([])}>clear all</button>
+        </h3>
+      </section>
+    </main>
+  )
+}
+
+export default App
