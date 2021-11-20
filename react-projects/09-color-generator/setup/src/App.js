c95adaf11cae0b36d37569f5fdecb144ce8a677d
ccm-chucky <charlescmatovu@gmail.com>
2021-11-20 17:42:34 -0500

    react project color generator,setup and App component

--- react-projects/09-color-generator/setup/src/App.js
+++ react-projects/09-color-generator/setup/src/App.js
@@ -4,7 +4,31 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color,setColor] = useState('ere');
+  const [error,setError] = useState(false);
+  const [list,setList] = useState([]);
+console.log('sdf');
+  const handleSubmit = (e)=>{
+    e.prevenDefault();
+    let colors = new Values(color).all(10);
+    console.log('the colores are the following'+colors);
+
+  };
+  return <>
+  <section className="container">
+    <h3>color gene</h3>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={color} onChange={(e)=>{
+        setColor(e.target.value)
+      }}
+      placeholder="#f15025"/>
+      <button className="btn" type="submit">submit</button>
+    </form>
+  </section>
+  <section className='colors'>
+    <h4>list here</h4>
+  </section>
+  </>
 }
 
 export default App
