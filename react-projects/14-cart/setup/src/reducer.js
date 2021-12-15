21dc7e6686c32872a0f44669614d9c41af0fa274
ccm-chucky <charlescmatovu@gmail.com>
2021-12-15 13:57:38 -0500

    cart final

--- react-projects/14-cart/setup/src/reducer.js
+++ react-projects/14-cart/setup/src/reducer.js
@@ -10,6 +10,44 @@ const reducer = (state,action)=>{
             item.id !== action.payload 
         )}
     }
+
+    if(action.type === 'INCREASE'){
+        let tempCart = state.cart.map((cartItem)=>{
+            if(cartItem.id === action.payload){
+                return {...cartItem,amount:cartItem.amount+1}
+            }
+            return cartItem
+        });
+        return {...state,cart:tempCart}
+    }
+    if (action.type === 'DECREASE') {
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 }
+        }
+        return cartItem
+      }).filter((cartItem)=>cartItem.amount !==0)
+      return { ...state, cart: tempCart }
+    }
+
+    if(action.type ==='GET_TOTALS'){
+        const{total,amount}=state.cart.reduce((cartTotal,cartItem)=>{
+            const {price,amount}=cartItem
+            const itemTotal = price * amount
+            cartTotal.amount += amount
+            cartTotal.total += itemTotal
+            return cartTotal
+        },{
+            total:0,
+            amount:0,
+        })
+
+       // total = parseFloat(total.toFixed(2))
+        return { ...state,total,amount}
+    }
+
+
+
     return state
 }
 
