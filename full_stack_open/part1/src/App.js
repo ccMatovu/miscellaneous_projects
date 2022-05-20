420c418d8778aa93b7f82f4e60fb57d2c5feed1a
ccm-chucky <charlescmatovu@gmail.com>
2022-05-20 09:53:56 -0400

    counter state

--- full_stack_open/part1/src/App.js
+++ full_stack_open/part1/src/App.js
@@ -1,6 +1,22 @@
-const App = (props) => {
-  const { counter } = props
-  return <div>counter = {counter}</div>
+import { useState } from 'react';
+
+const Display = (props) => {
+  return <div>{props.counter}</div>
 }
 
-export default App
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+    </div>
+  )
+}
+
+export default App;
--- full_stack_open/part1/src/index.js
+++ full_stack_open/part1/src/index.js
@@ -3,12 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-refresh()
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
