ab7ec73fb840ae497ab33da665cf7f2f5a57e058
ccm-chucky <charlescmatovu@gmail.com>
2022-05-20 09:18:01 -0400

    counter

--- full_stack_open/part1/src/index.js
+++ full_stack_open/part1/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './App'
 
+let counter = 1
 
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+refresh()
