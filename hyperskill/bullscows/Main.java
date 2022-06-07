5aa5e3418664a6f41d508cb21d9bc2e8bdd15431
ccm-chucky <charlescmatovu@gmail.com>
2022-06-07 18:44:27 -0400

    bull part 5/7

--- hyperskill/bullscows/Main.java
+++ hyperskill/bullscows/Main.java
@@ -2,34 +2,76 @@ package bullscows;
 import java.util.Scanner;
 import java.util.*;
 public class Main {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Please, enter the secret code's length:\n>");
+//        int digits = scan.nextInt();
+//        System.out.println("Okay, let's start a game!");
+//        ArrayList<Integer> randomNum = getRandomNumber(digits);
+//        ArrayList<Integer> ran2 = getRandomNumber2(digits);
+//        System.out.println("random number is = "+randomNum);
+//        System.out.println("random number is = ran 2 =  "+ran2);
+//        boolean notGuessed = true;
+//        int turns = 0;
+//        while (notGuessed){
+//            turns++;
+//            System.out.print("Turn "+turns+":\n> ");
+//            String[]  input = scan.next().split("");
+//            ArrayList<String> guess = new ArrayList<>(Arrays.asList(input));
+//           // System.out.println("guess is = "+guess);
+//            String result = grader(randomNum, guess);
+//            if(Character.getNumericValue(result.charAt(7)) == digits){
+//                System.out.println("Grade: 4 bulls\nCongratulations! You guessed the secret code.\n");
+//                notGuessed = false;
+//            }else{
+//                System.out.println(result);
+//            }
+//
+//        }
+//        //System.out.println("  digits = "+digits+"  rand= "+randomNum);
+//       // System.out.println("The random secret number is "+randomNum+".");
+//
+//    }
+public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Please, enter the secret code's length:\n>");
-        int digits = scan.nextInt();
-        System.out.println("Okay, let's start a game!");
-        ArrayList<Integer> randomNum = getRandomNumber(digits);
-        System.out.println("random number is = "+randomNum);
-        boolean notGuessed = true;
-        int turns = 0;
-        while (notGuessed){
-            turns++;
-            System.out.print("Turn "+turns+":\n> ");
-            String[]  input = scan.next().split("");
-            ArrayList<String> guess = new ArrayList<>(Arrays.asList(input));
-           // System.out.println("guess is = "+guess);
-            String result = grader(randomNum, guess);
-            if(Character.getNumericValue(result.charAt(7)) == digits){
-                System.out.println("Grade: "+digits+" bulls\nCongratulations! You guessed the secret code.\n");
-                notGuessed = false;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Please, enter the secret code's length:\n");
+    int digits = scan.nextInt();
+    String secretCode = getRandomNumber2(digits);
+    System.out.println("The random secret number is "+secretCode);
+
+
+    }
+
+    public static String getRandomNumber2(int digits){
+        if(digits > 10){
+            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+            return "";
+        }
+        ArrayList<Integer> number = new ArrayList<>();
+        String secret = "";
+        int length  = 0;
+        while(length < digits){
+            Random random = new Random();
+            int num = random.nextInt(10);
+            if(length == 0 && num == 0){
+                continue;
+            }
+            if(number.contains(num)){
+                continue;
             }else{
-                System.out.println(result);
+                number.add(num);
+                secret = secret+ num;
+                ++length;
             }
         }
+        return secret;
     }
     public static ArrayList<Integer> getRandomNumber(int digits){
         if(digits > 10){
             System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+            //return;
         }
         StringBuilder randomNumber = new StringBuilder();
         int numOfDigits = digits;
