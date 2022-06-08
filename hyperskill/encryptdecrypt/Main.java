645fbdd27094e6fdecc0db96a8913ee2ef5d88bd
ccm-chucky <charlescmatovu@gmail.com>
2022-06-08 18:02:38 -0400

    encrypt 2/7

--- hyperskill/encryptdecrypt/Main.java
+++ hyperskill/encryptdecrypt/Main.java
@@ -2,25 +2,108 @@ package encryptdecrypt;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String m = "we found a treasure!";
-        System.out.println(reverse(m));
+//        String m = "we found a treasure!";
+//        System.out.println(reverse(m));
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int amount = scanner.nextInt();
+        System.out.println(shift(input,amount));
+    }
+    public static String shift(String message,int key){
+        final int LETTERS_OF_ALPHABET = 26;
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i<message.length();i++){
+            char ch = message.charAt(i);
+            if(Character.isLetter(ch)){
+                String letter = String.valueOf(ch);
+                int position = getPosition(letter);
+                int newPosition = (position + key) % LETTERS_OF_ALPHABET;
+                letter = getLetter(newPosition);
+                sb.append(letter);
+            }else{
+                sb.append(String.valueOf(ch));
+            }
+        }
+        return sb.toString();
     }
     public static String reverse(String message){
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < message.length(); i++){
             char ch = message.charAt(i);
             if(Character.isLetter(ch)) {
-                sb.append(getCha("" + ch));
+                sb.append(getReverseLetter("" + ch));
             }else {
                 sb.append(""+ch);
             }
         }
         return sb.toString();
     }
-    public static String getCha(String letter) {
+
+    public static String getLetter(int position) {
+        Map<Integer,String> alphabet = new HashMap<>();
+        alphabet.put(1,"a");
+        alphabet.put(2,"b");
+        alphabet.put(3,"c");
+        alphabet.put(4,"d");
+        alphabet.put(5,"e");
+        alphabet.put(6,"f");
+        alphabet.put(7,"g");
+        alphabet.put(8,"h");
+        alphabet.put(9,"i");
+        alphabet.put(10,"j");
+        alphabet.put(11,"k");
+        alphabet.put(12,"l");
+        alphabet.put(13,"m");
+        alphabet.put(14,"n");
+        alphabet.put(15,"o");
+        alphabet.put(16,"p");
+        alphabet.put(17,"q");
+        alphabet.put(18,"r");
+        alphabet.put(19,"s");
+        alphabet.put(20,"t");
+        alphabet.put(21,"u");
+        alphabet.put(22,"v");
+        alphabet.put(23,"w");
+        alphabet.put(24,"x");
+        alphabet.put(25,"y");
+        alphabet.put(26,"z");
+        return alphabet.get(position);
+    }
+    public static Integer getPosition(String letter) {
+        Map<String,Integer> alphabet = new HashMap<>();
+        alphabet.put("a",1);
+        alphabet.put("b",2);
+        alphabet.put("c",3);
+        alphabet.put("d",4);
+        alphabet.put("e",5);
+        alphabet.put("f",6);
+        alphabet.put("g",7);
+        alphabet.put("h",8);
+        alphabet.put("i",9);
+        alphabet.put("j",10);
+        alphabet.put("k",11);
+        alphabet.put("l",12);
+        alphabet.put("m",13);
+        alphabet.put("n",14);
+        alphabet.put("o",15);
+        alphabet.put("p",16);
+        alphabet.put("q",17);
+        alphabet.put("r",18);
+        alphabet.put("s",19);
+        alphabet.put("t",20);
+        alphabet.put("u",21);
+        alphabet.put("v",22);
+        alphabet.put("w",23);
+        alphabet.put("x",24);
+        alphabet.put("y",25);
+        alphabet.put("z",26);
+        return alphabet.get(letter);
+    }
+    public static String getReverseLetter(String letter) {
         Map<String,String> alphabet = new HashMap<>();
         alphabet.put("a","z");
         alphabet.put("b","y");
