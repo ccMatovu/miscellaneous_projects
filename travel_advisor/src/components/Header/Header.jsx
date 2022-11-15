2c02e98babb7c8b4ad2dd5a9f9148f44e2c318f5
ccm-chucky <charlescmatovu@gmail.com>
2022-11-15 18:03:56 -0500

    Layout and syles

--- travel_advisor/src/components/Header/Header.jsx
+++ travel_advisor/src/components/Header/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
 
 const Header =() =>{
-    return (<h1>Header</h1>);
+    return (<AppBar position = "static">
+        <Toolbar className={classes.toobar}>
+            <Typography variant ="h5" className={classes.title}>
+                Travel Adisor
+            </Typography>
+            <Box display = "flex"></Box>
+            <Typography variant="h6" className={classes.tille}>
+                Explore new places
+            </Typography>
+            <Autocomplete>
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon/>
+                    </div>
+                    <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                </div>
+            </Autocomplete>
+        </Toolbar>
+    </AppBar>);
 }
 
 export default Header;
