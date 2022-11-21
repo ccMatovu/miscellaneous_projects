3e95684d3ef0e9234cfcc37daa07cf9f46686760
ccm-chucky <charlescmatovu@gmail.com>
2022-11-21 16:21:09 -0500

    styles

--- travel_advisor/src/components/Header/Header.jsx
+++ travel_advisor/src/components/Header/Header.jsx
@@ -1,31 +1,38 @@
-import React from "react";
-import { Autocomplete } from "@react-google-maps/api";
-import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
+import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 
-import makeStyles from './Headerstyles';
+import useStyles from './styles.js'
 
-const Header =() =>{
-    const classes = makeStyles();
-    return (<AppBar position = "static">
-        <Toolbar className={classes.toobar}>
-            <Typography variant ="h5" className={classes.title}>
-                Travel Adisor
-            </Typography>
-            <Box display = "flex"></Box>
-            <Typography variant="h6" className={classes.tille}>
-                Explore new places
-            </Typography>
-           {/* <Autocomplete>*/}
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <SearchIcon/>
-                    </div>
-                    <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
-                </div>
-           {/* </Autocomplete>*/}
-        </Toolbar>
-    </AppBar>);
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles()
+
+  return (
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h5' className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display='flex'>
+          <Typography variant='h6' className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder='Search…'
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
-export default Header;
+export default Header
