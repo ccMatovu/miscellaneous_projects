5020c63fcbb543c169157c06e9e2e89420d5307b
ccm-chucky <charlescmatovu@gmail.com>
2022-11-21 16:25:32 -0500

    PlaceDetails

--- travel_advisor/src/components/Map/Map.jsx
+++ travel_advisor/src/components/Map/Map.jsx
@@ -1,42 +1,92 @@
-import { useMemo } from 'react';
-import { useLoadScript, Marker } from '@react-google-maps/api';
-import GoogleMapReact  from 'google-map-react';
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab/Rating'
 
-export default function Home() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyAsRhrnYxNcZO-o0WKROEPimDh4UkWh7EI',
-  })
-  console.log("loasdd");
+import mapStyles from '../../mapStyles'
+import useStyles from './styles.js'
 
-  if (!isLoaded) return <div>Loading...</div>
-  return <>
-  <div>now</div>
-  <Map googleMapsApiKey />
-  <div>after</div>
-  </>
-}
+const Map = ({
+  coords,
+  places,
+  setCoords,
+  setBounds,
+  setChildClicked,
+  weatherData,
+}) => {
+  const matches = useMediaQuery('(min-width:600px)')
+  const classes = useStyles()
 
-function Map({}) {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyAsRhrnYxNcZO-o0WKROEPimDh4UkWh7EI',
-  })
-  console.log('loasdd')
-
-  if (!isLoaded) return <div>Loading...</div>
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), [])
-  console.log("maphere");
-
-  return (<>
-  <div>here</div>
-    <GoogleMapReact
-      zoom={10}
-      center={center}
-      mapContainerClassName='map-container'
-      bootstrapURLKeys={{ key: {googleMapsApiKey} }}
-    >
-      <Marker position={center} />
-    </GoogleMapReact>
-    </>
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng })
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places.length &&
+          places.map((place, i) => (
+            <div
+              className={classes.markerContainer}
+              lat={Number(place.latitude)}
+              lng={Number(place.longitude)}
+              key={i}
+            >
+              {!matches ? (
+                <LocationOnOutlinedIcon color='primary' fontSize='large' />
+              ) : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography
+                    className={classes.typography}
+                    variant='subtitle2'
+                    gutterBottom
+                  >
+                    {' '}
+                    {place.name}
+                  </Typography>
+                  <img
+                    className={classes.pointer}
+                    src={
+                      place.photo
+                        ? place.photo.images.large.url
+                        : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+                    }
+                  />
+                  <Rating
+                    name='read-only'
+                    size='small'
+                    value={Number(place.rating)}
+                    readOnly
+                  />
+                </Paper>
+              )}
+            </div>
+          ))}
+        {weatherData?.list?.length &&
+          weatherData.list.map((data, i) => (
+            <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+              <img
+                src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                height='70px'
+              />
+            </div>
+          ))}
+      </GoogleMapReact>
+    </div>
   )
-  
 }
+
+export default Map
